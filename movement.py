12c1060e6a765e96b3ab05d73dbfb07e2f81737b
Rembert Muylle <74431619+Rembie01@users.noreply.github.com>
2020-12-06 18:23:16 +0100

    Added rudimentary movement controls
    movement is still wip

--- movement.py
+++ movement.py
@@ -1,29 +1,57 @@
 import numpy as np
+import math
 
 
 class Movement:
     """Allows the stepper motor to move"""
 
-    def __init__(self, lamda, focal_length, rho):
+    def __init__(self, lamda, focal_length, rho, resolution):
         # Lamda (lambda) is a multiplying factor for the speed at which the camera should rotate
         self.lamda = lamda
         # Focal length is the focal length of the camera used
         self.focal_length = focal_length
         # Rho is the pixel size
         self.rho = rho
+        # Resolution is the resolution of the camera
+        if resolution == tuple or list:
+            self.resolution = resolution
 
-    def move_left(self, xcoord, ycoord):
-        jp = np.a
-        return 't:00000r:' + rotate + 's:' + rpm
+    def move_horizontal(self, xcoord, ycoord):
+        u = xcoord
+        v = ycoord
+        coord = (u, v)
+        center = (int(self.resolution[0]/2), int(self.resolution[1]/2))
+        pe = (coord[0] - center[0], coord[1] - center[1])
+        l = self.lamda
+        f = self.focal_length
+        r = self.rho
+        jp = np.array([[(r*u*v)/f, -(f*f+r*r*u*u)/(r*f)],
+                       [(f*f+r*r*v*v)/(r*f), -(r*u*v)/f]])
+        inv_jp = np.linalg.pinv(jp)
+        speed = l*inv_jp*pe
+        rpm = speed*2*math.pi/60
+        rotate = 'XXXXX'
+        print(rpm)
+        return 't:00000r:' + rotate + 's:' + str(round(rpm[1][0]))
 
-    def move_right(self, xcoord, ycoord):
+    def move_vertical(self, xcoord, ycoord):
+        u = xcoord
+        v = ycoord
+        coord = (u, v)
+        center = (int(self.resolution[0] / 2), int(self.resolution[1] / 2))
+        pe = (coord[0] - center[0], coord[1] - center[1])
+        l = self.lamda
+        f = self.focal_length
+        r = self.rho
+        jp = np.array([[(r * u * v) / f, -(f * f + r * r * u * u) / (r * f)],
+                       [(f * f + r * r * v * v) / (r * f), -(r * u * v) / f]])
+        inv_jp = np.linalg.pinv(jp)
+        speed = l * inv_jp * pe
+        rpm = speed * 2 * math.pi / 60
+        print(rpm)
+        return 't:' + tilt + 'r:00000s:' + str(rpm[0])
 
-        return 't:00000r:' + rotate + 's:' + rpm
 
-    def move_up(self, xcoord, ycoord):
+test = Movement(1.5, 60, 0.003, (1280, 720))
 
-        return 't:' + tilt + 'r:00000s:' + rpm
-
-    def move_down(self, xcoord, ycoord):
-
-        return 't:' + tilt + 'r:00000s:' + rpm
+print(test.move_horizontal(640, 360))
---
+++ rudimentary_movement.py
@@ -0,0 +1,42 @@
+class Movement:
+    """Allows the stepper motor to move"""
+
+    def __init__(self, factor=1, fov_angle_horizontal=62, fov_angle_vertical=37, resolution=(1280, 720), play=2):
+        self.factor = factor
+        self.h_angle = fov_angle_horizontal
+        self.v_angle = fov_angle_vertical
+        self.res = resolution
+        self.play = play
+        self._direction = '-'
+
+    def move_horizontal(self, xcoord):
+        rotate_pixels = -(xcoord - self.res[0]/2)
+        pixels_per_degree = self.res[0] / self.h_angle
+        rotate = round(rotate_pixels / pixels_per_degree)
+        if rotate < 0:
+            self._direction = '+'
+            rotate = -rotate
+        if rotate <= self.play:
+            return None
+        if len(str(rotate)) <= 1:
+            rotate = '0' + str(rotate)
+        rpm = self.factor * rotate
+        while len(str(rpm)) <= 3:
+            rpm = '0' + str(rpm)
+        return 't:00r:' + str(rotate) + 's:' + str(rpm) + 'td:0rd:' + self._direction
+
+    def move_vertical(self, ycoord):
+        tilt_pixels = -(ycoord - self.res[1] / 2)
+        pixels_per_degree = self.res[1] / self.v_angle
+        tilt = round(tilt_pixels / pixels_per_degree)
+        if tilt < 0:
+            self._direction = '+'
+            tilt = -tilt
+        if tilt <= self.play:
+            return None
+        if len(str(tilt)) <= 1:
+            tilt = '0' + str(tilt)
+        rpm = self.factor * tilt
+        while len(str(rpm)) <= 3:
+            rpm = '0' + str(rpm)
+        return 't:' + str(tilt) + 'r:00s:' + str(rpm) + 'td:' + self._direction + 'rd:0'
