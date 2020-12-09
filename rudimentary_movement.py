42345f3a769e54a42136703803c9bdaad39b0322
Rembie01 <rembert.muylle@gmail.com>
2020-12-09 11:17:26 +0100

    Added a move function
    allows the rotate and tilt to be sent in 1 time

--- rudimentary_movement.py
+++ rudimentary_movement.py
@@ -17,23 +17,58 @@ class Movement:
         pixels_per_degree = self.res[0] / self.h_angle
         rotate = round(rotate_pixels / pixels_per_degree)
         if rotate <= self.play:
-            return '00.00.0000'
+            return '00,00,0000'
         if len(str(rotate)) <= 1:
             rotate = '0' + str(rotate)
         rpm = self.factor * rotate
         while len(str(rpm)) <= 3:
             rpm = '0' + str(rpm)
-        return '00.' + str(rotate) + '.' + str(rpm)
+        return '00,' + str(rotate) + ',' + str(rpm)
 
     def move_vertical(self, ycoord):
         tilt_pixels = -(ycoord - self.res[1] / 2)
         pixels_per_degree = self.res[1] / self.v_angle
         tilt = round(tilt_pixels / pixels_per_degree)
         if tilt <= self.play:
-            return '00.00.0000'
+            return '00,00,0000'
         if len(str(tilt)) <= 1:
             tilt = '0' + str(tilt)
         rpm = self.factor * tilt
         while len(str(rpm)) <= 3:
             rpm = '0' + str(rpm)
-        return str(tilt) + '.00.' + str(rpm)
+        return str(tilt) + ',00,' + str(rpm)
+
+    def move(self, xcoord, ycoord):
+        rotate_pixels = -(xcoord - self.res[0] / 2)
+        tilt_pixels = -(ycoord - self.res[1] / 2)
+        rotate_pixels_per_degree = self.res[0] / self.h_angle
+        tilt_pixels_per_degree = self.res[1] / self.v_angle
+        rotate = round(rotate_pixels / rotate_pixels_per_degree)
+        tilt = round(tilt_pixels / tilt_pixels_per_degree)
+        if rotate <= self.play and tilt <= self.play:
+            return '00,00,0000,0000'
+        if rotate <= self.play:
+            if len(str(tilt)) <= 1:
+                tilt = '0' + str(tilt)
+            tilt_rpm = self.factor * tilt
+            while len(str(tilt_rpm)) <= 3:
+                tilt_rpm = '0' + str(tilt_rpm)
+            return str(tilt) + ',00,' + str(tilt_rpm) + ',0000'
+        if tilt <= self.play:
+            if len(str(rotate)) <= 1:
+                rotate = '0' + str(rotate)
+            rotate_rpm = self.factor * rotate
+            while len(str(rotate_rpm)) <= 3:
+                rotate_rpm = '0' + str(rotate_rpm_rpm)
+            return '00,' + str(rotate) + ',0000,' + str(rotate_rpm)
+        if len(str(rotate)) <= 1:
+            rotate = '0' + str(rotate)
+        if len(str(tilt)) <= 1:
+            tilt = '0' + str(tilt)
+        rotate_rpm = self.factor * rotate
+        tilt_rpm = self.factor * tilt
+        while len(str(rotate_rpm)) <= 3:
+            rotate_rpm = '0' + str(rotate_rpm)
+        while len(str(tilt_rpm)) <= 3:
+            tilt_rpm = '0' + str(tilt_rpm)
+        return str(tilt) + ',' + str(rotate) + ',' + str(tilt_rpm) + ',' + str(rotate_rpm)
